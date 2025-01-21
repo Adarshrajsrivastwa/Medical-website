904129c72e75994bab467907ba02f9906427991b
Harshit Patle <harshitpatle1530@gmail.com>
2025-01-21 16:18:39 +0530

    Add Few Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,37 @@
+import { Button } from "@/components/ui/button"
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 
 
 function App() {
 
   return (
-    <div>Hello world</div>
+    <div className="flex flex-col items-center justify-center h-screen gap-5">
+      <Button variant="ours">Click me</Button>
+      <InputOTP maxLength={6}>
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+        </InputOTPGroup>
+        <InputOTPSeparator />
+        <InputOTPGroup>
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+      <Label htmlFor="email">Full Name</Label>
+      <Input type="text" placeholder="Enter your name" className="w-40" />
+    </div>
+
   )
 }
 
