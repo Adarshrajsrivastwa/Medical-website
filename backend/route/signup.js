ab8d2968fb528e59319baa00383c4820098a7917
Adarshrajsrivastwa <srivastwaadarsh@gmail.com>
2025-01-23 00:24:05 +0530

    email sender completed

--- backend/route/signup.js
+++ backend/route/signup.js
@@ -4,34 +4,39 @@ const bodyParser = require('body-parser');
 
 let app = express();
 
-// // OTP generate karne ke liye function
-// function generateOTP() {
-//   return Math.floor(100000 + Math.random() * 900000); 
-// }
+function generateOTP() {
+  return Math.floor(100000 + Math.random() * 900000); 
+}
 
-// let transporter = nodemailer.createTransport({
-//   service: 'gmail', 
-//   auth: {
-//     user: 'adarsh6205840092@gmail.com', 
-//     pass: 'Adarsh@9471'   
-//   }
-// });
+let transporter = nodemailer.createTransport({
+  service: 'gmail', 
+  host:"smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'srivastwaadarsh@gmail.com', 
+    pass: 'hkvw vikl mqdp pcoz'   
+  }
+});
 
-// let otp = generateOTP();
+let otp = generateOTP();
 
-// let mailOptions = {
-//   from: 'adarsh6205840092@gmail.com',
-//   to: 'recipient_email@example.com',  
-//   subject: 'Your OTP for Authentication', 
-//   html: `<h3>Your OTP is: <strong>${otp}</strong></h3><p>Use this code to authenticate your login.</p>` 
-// };
+let mailOptions = {
+  from:{
+    name: 'CareSpaceX',
+    address: 'srivastwaadarsh@gmail.com',
+  },
+  to: 'harshitpatle585@gmail.com',  
+  subject: 'Your OTP for Authentication', 
+  html: `<h3>Your OTP is: <strong>${otp}</strong></h3><p>Use this code to authenticate your login.</p>` 
+};
 
-// transporter.sendMail(mailOptions, (error, info) => {
-//   if (error) {
-//     return console.log('Error:', error);
-//   }
-//   console.log('Email sent:', info.response);
-// });
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    return console.log('Error:', error);
+  }
+  console.log('Email sent:', info.response);
+});
 
 app.post('/signup', (req, res) => {
     console.log("hello from backend");
