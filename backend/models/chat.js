540610dc883d614168b0b591286351383a2ff2ff
Adarshrajsrivastwa <srivastwaadarsh@gmail.com>
2025-01-31 03:06:53 +0530

    chat save in database

--- backend/models/chat.js
+++ backend/models/chat.js
@@ -2,22 +2,27 @@ const mongoose = require('mongoose');
 let connectdb= require('../config/db');
 const { model } = mongoose; 
 
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, unique: true, required: true },
-    data: {
-      chat_history: [
-        {
-          timestamp: { type: Date, default: Date.now },
-          message: String,
-          sender:{ type: String, default:"recieved"}
-        },
-      ],
-      preferences: {
-        language: { type: String, default: "English" },
-        theme: { type: String, default: "Light" },
-      },
-    },
-  });
-  
-  const Chat = mongoose.model("Chat", userSchema);
+const chartSchema = new mongoose.Schema({
+  sender: {
+    type: String,  
+    required: true,
+  },
+  recipient: {
+    type: String, 
+    required: true,
+  },
+  timestamp: {
+    type: Date,  
+    required: true,
+  },
+  message: {
+    type: String,  
+    required: true,
+  },
+}, {
+  timestamps: true,  
+});
+
+const Chart = mongoose.model('Chart', chartSchema);
+
+module.exports = Chart;
